e31e9e94b682a39c87ca08db9fe56dd0641a897e
prateeksha17 <112319597+prateeksha17@users.noreply.github.com>
2024-01-16 01:41:28 +0530

    Add files via upload

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Prateeksha Pandey </span>
+            from <span className="purple"> Lucknow, India.</span>
+            <br />
+            I am currently persuing Civil Engineering in Dr. BR Ambedkar National Institute Of Technology Jalandhar.
+           
+            
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Embriodry
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Content
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          {/* <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Soumyajit</footer> */}
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,123 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import image1 from '../../Assets/Projects/image1.png';
+import image2 from '../../Assets/Projects/image2.png';
+import image3 from '../../Assets/Projects/image3.png';
+import image4 from '../../Assets/Projects/image4.png';
+import image5 from '../../Assets/Projects/image5.png';
+import image6 from '../../Assets/Projects/image6.png';
+import image7 from '../../Assets/Projects/image7.png';
+import image8 from '../../Assets/Projects/image8.png';
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image1}
+              isBlog={false}
+              title="Memory Game"
+              description="Memory game: Test your recall with matching pairs. Flip cards to find matches and improve memory. Great for all ages, enhancing cognitive skills and entertaining fun. 
+              Tech Stack: React JS, Javascripts, HTML, CSS"
+              ghLink="https://github.com/prateeksha17/memory_cards.github.io"
+              //demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image2}
+              isBlog={false}
+              title="Recipe site"
+              description=
+              "Elevate your culinary skills with our recipe site! Explore diverse cuisines, step-by-step instructions, and user-friendly interface. Unleash your inner chef and savor delicious homemade creations.Tech Stack: React JS, Javascripts, HTML, CSS, Firebase"
+              ghLink="https://github.com/prateeksha17/Lets_cook.github.io"
+              //demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image3}
+              isBlog={false}
+              title="Money Tracker"
+              description="Take control of your finances with our money tracking site. Effortlessly manage expenses, set budgets, and gain financial insight. Empower yourself to achieve financial goals and peace of mind.
+              Tech Stack: React JS, Javascripts, HTML, CSS, Firebase"
+              ghLink="https://github.com/prateeksha17/portfolio.github.io"
+              //demoLink="https://editor.soumya-jit.tech/"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image4}
+              isBlog={false}
+              title="Task Management Site"
+              description="Boost productivity with our task management site. Organize, prioritize, and collaborate seamlessly. Streamline workflows, track progress, and achieve more. Elevate your efficiency and conquer your to-do list effortlessly. Tech Stack: React JS, Javascripts, HTML, CSS, Firebase"
+              ghLink="https://github.com/prateeksha17/Dojo.github.io"
+              //demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image5}
+              isBlog={false}
+              title="OyakuFeed"
+              description="Connect with fellow anime enthusiasts on our social media site. Share favorite shows, fan art, and discussions. Embrace the anime community, discover new series, and celebrate the love for Japanese animation. Tech Stack: React JS, Javascripts, HTML, CSS, Express Js, Node JS, MongoDB"
+              ghLink="https://github.com/prateeksha17/OtakuFeed.github.io"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+            />
+          </Col>
+
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image6}
+              isBlog={false}
+              title="portfolio"
+              description="Showcase your talent with our portfolio site. Display your work elegantly, attractively, and professionally. Impress clients and employers with a stunning visual representation of your skills and achievements. Tech Stack: React JS, Javascripts, HTML, CSS, Node Js"
+              
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image7}
+              isBlog={false}
+              title="Blogify"
+              description="Elevate your voice with our blog creation site. Effortlessly craft, customize, and publish content. Engage your audience, build a community, and amplify your impact with a platform designed for success. Tech Stack: Editor.js, Javascripts, HTML, CSS, Firebase"
+              ghLink="https://github.com/prateeksha17/blogify.github.io"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image8}
+              isBlog={false}
+              title="Callets"
+              description="Experience seamless communication with our video conferencing site. Connect, collaborate, and engage with crystal-clear video and audio. Enhance productivity and foster meaningful connections with a reliable, user-friendly platform. Tech Stack:  Javascripts, HTML, CSS, Agora"
+              ghLink="https://github.com/prateeksha17/callets.github.io"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
