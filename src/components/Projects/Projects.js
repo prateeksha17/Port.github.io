7ca4e4c319251649bbbb5c2c8011a572a6d93a1b
prateeksha17 <prateekshapandey8177@gmail.com>
2024-08-10 00:57:34 +0530

    initial commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,11 +4,11 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import image1 from '../../Assets/Projects/image1.png';
 import image2 from '../../Assets/Projects/image2.png';
-import image3 from '../../Assets/Projects/image3.png';
+
 import image4 from '../../Assets/Projects/image4.png';
 import image5 from '../../Assets/Projects/image5.png';
-import image6 from '../../Assets/Projects/image6.png';
-import image7 from '../../Assets/Projects/image7.png';
+import image10 from '../../Assets/Projects/image10.png';
+import image9 from '../../Assets/Projects/image9.png';
 import image8 from '../../Assets/Projects/image8.png';
 
 function Projects() {
@@ -83,6 +83,28 @@ function Projects() {
               title="Callets"
               description="Experience seamless communication with our video conferencing site. Connect, collaborate, and engage with crystal-clear video and audio. Enhance productivity and foster meaningful connections with a reliable, user-friendly platform. Tech Stack:  Javascripts, HTML, CSS, Agora"
               ghLink="https://github.com/prateeksha17/callets.github.io"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image9}
+              isBlog={false}
+              title="SyntaxStories"
+              description="Syntax Stories is a project created with the aim of combining storytelling with coding concepts. The project likely serves as an educational tool that makes learning programming languages more engaging by weaving syntax and code snippets into narrative form. It might explore various coding topics or algorithms through a creative, story-driven approach, making complex ideas more relatable and easier to understand."
+              ghLink="https://github.com/prateeksha17/mern-blog-main"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image10}
+              isBlog={false}
+              title="SortingVisualiser"
+              description="A Sorting Visualizer is an interactive tool or application designed to visually demonstrate how different sorting algorithms work. It typically displays a set of data (like bars of varying heights) and animates the process of sorting this data step by step, allowing users to observe how elements are compared, swapped, or moved according to the rules of the specific sorting algorithm being visualized."
+              ghLink="https://github.com/prateeksha17/Sorting-Visualizer-main"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
